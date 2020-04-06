9c3d6475ebbb3b8c8b7b64cdd3e091a02a3d864f
franco-canedo <francanedo1@hotmail.com>
2020-04-06 15:56:33 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,24 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <ul>
+      <li><NavLink 
+        to="/"
+        exact
+      >Home</NavLink>
+      <li><NavLink 
+        to="/movies"
+        exact
+      >Movies</NavLink></li>
+      <NavLink 
+        to="/directors"
+        exact
+      >Directors</NavLink></li>
+      <li><NavLink 
+        to="/actors"
+        exact
+      >Actors</NavLink></li>
+      </ul>
     </div>
   );
 };
